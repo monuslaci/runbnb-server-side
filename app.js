e70a9106b7096e374647b5a53bf1f65d17b238cb
Lali Micskei <71842354+micskeil@users.noreply.github.com>
2021-03-24 09:39:29 +0100

    fix userId requirement for listing Schema

--- app.js
+++ app.js
@@ -13,28 +13,14 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+
 app.get("/", (req, res, next) => {
   console.log("OK");
 });
 
-//routes
-//Listing
+
 const listingRoutes = require("./routes/listing");
 app.use("/listings", listingRoutes);
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://runbnb:runbnb9876@runbnb.ffxee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-//   )
-//   .then((result) => {
-//     console.log(result);
-//     app.listen(port);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-
 
 
 mongoose.connect(process.env.MONGO_URI, {
